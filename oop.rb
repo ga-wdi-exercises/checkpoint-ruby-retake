760578250848932eb45dad06b4845dc1e440686d
Christian <xianlanoue@gmail.com>
2017-09-08 14:28:44 -0400

    oop

--- oop.rb
+++ oop.rb
@@ -5,10 +5,29 @@
 # - The ability to "get" but NOT "set" `color`
 # Type your solution directly below this line:
 
+class Animal
+  attr_accessor :color
+
+   def initialize(color)
+     @color = color
+   end
+
+   def eat
+
+   end
+
+   def get_color
+     return @color
+   end
+
+end
+
 
 # Question 7
 # Create a new `Animal` instance with the color "brown".
 # Type your solution directly below this line:
+
+change = Animal.new('brown')
 
 
 # Question 8
@@ -19,9 +38,30 @@
 # - A breed attribute that you are able to "get" and "set"
 # Type your solution directly below this line:
 
+class Dog < Animal
+  attr_accessor :color, :breed
+
+   def initialize(name, king)
+     @color = color
+   end
+
+   def bark
+     "bark bark"
+   end
+
+   def get_breed
+     return @breed
+   end
+
+   def set_breed(breed)
+     @breed = breed
+   end
+ end
 
 
 
 # Question 9
 # Create a new instance of `Dog` with the color "black" and the breed "labrador retriever".
 # Type your solution directly below this line:
+dark = Dog.new.color("black")
+good_boy = Dog.new.breed("labrador retriever")
