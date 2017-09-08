9907d3c7868ac1a5d850f1a3b3e6f4519c8b6261
superbuggy <jpqreichard@gmail.com>
2017-09-08 10:52:12 -0400

    amend test

--- spec/2-enums_spec.rb
+++ spec/2-enums_spec.rb
@@ -34,6 +34,7 @@ describe "Enums #5" do
   end
 
   it "Each age has been increased by 30 years" do
-    expect(older_ages.all?{ |age| age > 30 }).to eq(true)
+    expected_ages = [41, 39, 40, 38, 39]
+    expect(older_ages).expect match_array(expected_ages)
   end
 end
